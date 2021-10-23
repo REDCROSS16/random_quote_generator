a03a172bf6c84bbeddfbd553bb8c37c33109ac74
redcross16 <belkill@mail.ru>
2021-10-23 03:50:39 +0300

    random_quote_generator

---
+++ script.js
@@ -0,0 +1,21 @@
+let quote = document.querySelector('#quote');
+let author = document.querySelector('#author');
+let btn = document.querySelector('#btn');
+
+
+const url = 'http://api.quotable.io/random';
+
+let getQuote = () => {
+    fetch(url)
+        .then((data) => data.json())
+        .then((item) => {
+            console.log(item.content);
+            console.log(item.author);
+            quote.innerText = item.content;
+            author.innerText = item.author;
+        })
+}
+
+
+window.addEventListener('load', getQuote);
+btn.addEventListener('click', getQuote)
